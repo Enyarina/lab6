b0bdad3d99de86b80fcf16896f886b9752649c93
greendilow <54859905+greendilow@users.noreply.github.com>
2020-03-28 09:54:03 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,21 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
-
-#endif // INCLUDE_HEADER_HPP_
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <picosha2.h>
+#include <future>
+#include <thread>
+#include <vector>
+#include <boost/log/trivial.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#endif  // INCLUDE_HEADER_HPP_
